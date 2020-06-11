1b81d5aa5b3bb94dddacd0339841fb1f97138abb
Theresa Rutledge <t.roserutledge@gmail.com>
2020-06-11 10:47:26 -0700

    high scores added

--- assets/script.js
+++ assets/script.js
@@ -30,11 +30,22 @@ const startBtn = document.querySelector('#startBtn');
 const questionTitle = document.querySelector('.question');
 const answerOptions = document.querySelector('.answer-options');
 const resultContainer = document.querySelector('.result');
+const main = document.querySelector('main');
+const highScoresBtn = document.querySelector('.high-score');
 
 let questionContainer = document.querySelector('.question-container');
 let currentQuestion = 0;
 let currentScore = 0;
 let highScores = [];
+
+const loadScores = () => {
+    highScores = localStorage.getItem('scores');
+    if(!highScores){
+        highScores = [];
+    } else {
+        highScores = JSON.parse(highScores);
+    }
+}
 
 //generate question from array
 const startQuiz = () => {
@@ -74,6 +85,7 @@ const checkAnswer = (event) => {
     
    userAnswer = parseInt(userAnswer);
     if(userAnswer === correctAnswer){
+        currentScore++;
         showResult(true);
     } else {
         showResult(false);
@@ -141,12 +153,15 @@ const endGame = () => {
     initialsSubmit.addEventListener('click',function (){
         event.preventDefault();
         initialsValue = initialsInput.value;
+        initialsValue = initialsValue.toUpperCase();
         if(!validateInitials(initialsValue)){
             alert('Enter two letter initials');
             initialsForm.reset();
             return false;
         }
+       
         highScores.push({name:initialsValue,score:currentScore});
+        saveScores();
         viewHighScores();
        
     });
@@ -162,7 +177,40 @@ const validateInitials = (string) => {
         return true;
     }
 }
+//save scores to local
+const saveScores = () => {
+    localStorage.setItem('scores',JSON.stringify(highScores));
+}
 
+//generate high scores list
+const viewHighScores = () => {
+main.innerHTML = '';
+let scoresTitle = document.createElement('h2');
+scoresTitle.textContent = 'High Scores';
+main.appendChild(scoresTitle);
+
+for (i=0;i<highScores.length; i++){
+    let score = document.createElement('h4');
+    score.innerText = `${highScores[i].name}: ${highScores[i].score}`;
+    main.appendChild(score);
+}
+
+let startOver = document.createElement('button');
+startOver.className = 'btn';
+startOver.textContent = 'Take Quiz';
+startOver.setAttribute('id','start-over');
+main.appendChild(startOver);
+
+startOver.addEventListener('click', function (){
+    location.reload();
+});
+
+
+}
+
+loadScores();
 //show first question when start button is clicked
 startBtn.addEventListener('click',startQuiz);
+
+highScoresBtn.addEventListener('click',viewHighScores);
 
